5b6f6de7c16d0464a22afc1f5fadc2ac139ae40b
suryansh9315 <suryansh.sharma9315@gmail.com>
2026-03-13 04:30:00 +0530

    fix: Fixed searchparam error

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,23 @@
-import BookCard from "@/components/BookCard";
-import HeroSection from "@/components/HeroSection";
-import Search from "@/components/Search";
 import React from "react";
+import HeroSection from "@/components/HeroSection";
+import BookCard from "@/components/BookCard";
 import { getAllBooks } from "@/lib/actions/book.actions";
+import Search from "@/components/Search";
 
-const Page = async () => {
-  const bookResults = await getAllBooks();
+const Page = async ({
+  searchParams,
+}: {
+  searchParams: Promise<{ query?: string }>;
+}) => {
+  const { query } = await searchParams;
+
+  const bookResults = await getAllBooks(query);
   const books = bookResults.success ? (bookResults.data ?? []) : [];
 
   return (
     <main className="wrapper container">
       <HeroSection />
+
       <div className="flex flex-col sm:flex-row sm:items-center justify-between gap-5 mb-10">
         <h2 className="text-3xl font-serif font-bold text-[#212a3b]">
           Recent Books
